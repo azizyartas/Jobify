564af19725af8953b76ebf2a791eec9ae246279b
azizyartas <azizyartas18@gmail.com>
2024-07-19 14:57:44 +0300

    day 21&22

---
+++ resources/views/components/form-error.blade.php
@@ -0,0 +1,7 @@
+@props(['name'])
+
+@error($name)
+    <p {{ $attributes->merge(['class' => 'text-xs text-red-500 font-semibold mt-1']) }}>
+        {{ $message }}
+    </p>
+@enderror
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', 'App\Http\Controllers\JobController');
+
+// Auth
+Route::get('/register', 'App\Http\Controllers\RegisteredUserController@create');
+Route::post('/register', 'App\Http\Controllers\RegisteredUserController@store');
+
+Route::get('/login', 'App\Http\Controllers\SessionController@create');
+Route::post('/login', 'App\Http\Controllers\SessionController@store');
+Route::post('/logout', 'App\Http\Controllers\SessionController@destroy');
